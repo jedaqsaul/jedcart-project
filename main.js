5fdcf83cf92f41e5c4a41282269856acec0c7e42
jedidiah <jedaqsaul@gmail.com>
2025-03-26 22:33:24 +0300

    Updated db.json file

---
+++ main.js
@@ -0,0 +1,11 @@
+// fetch("http://localhost:3000/products")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     data.forEach((product) => console.log(product.description));
+//   });
+
+// fetch("http://localhost:3000/products")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data[0].description);
+//   });
