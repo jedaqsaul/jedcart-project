52a4e82fdcd7af9e88b850b3c489bb607242acc3
jedidiah <jedaqsaul@gmail.com>
2025-03-27 09:57:41 +0300

    Added function to fetch products from db.json

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Load Dom and select all elements
 document.addEventListener("DOMContentLoaded", () => {
   const productsContainer = document.querySelector(".products-center");
   const cartContent = document.querySelector(".cart-content");
@@ -10,3 +11,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let cart = [];
 });
+
+//fetch prouducts from db.json
+
+async function fetchProducts() {
+  try {
+    const response = await fetch("http://localhost:3000/products");
+    const products = await response.json();
+    //call the function to display products
+    displayProducts(products);
+  } catch (error) {
+    console.error(error);
+  }
+}
+console.log(fetchProducts());
